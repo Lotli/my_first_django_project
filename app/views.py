81bf8b355b594c0367a78dff1dd40e8cd7f32647
Lotli <andrey@shanalin.ru>
2025-11-03 21:00:56 +0300

    Created product cards and split them into categories

--- app/views.py
+++ app/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
 from .models import Product, Brand, Category
 
-# Create your views here.
 def index(request):
-
     products = Product.objects.all()
     brands = Brand.objects.all()
-    categories = Brand.objects.all()
+    categories = Category.objects.all()
 
     context = {
         "products": products,
-        "brands" : brands,
-        "categories" : categories,
+        "brands": brands,
+        "categories": categories,
     }
-
-    return render(request, "app/index.html")
+    return render(request, "app/index.html", context)
